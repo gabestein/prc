556cfe6dab553e9ebc1990b42892a06974954155
Gabriel Stein <g@gabestein.com>
2020-01-15 12:18:16 -0500

    new: Add graphql fetching

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import App from 'next/app';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import withData from '../util/apollo-client';
+
+class MyApp extends App {
+	render() {
+		const { Component, pageProps, apollo } = this.props;
+		return (
+			<ApolloProvider client={apollo}>
+				<Component
+					// eslint-disable-next-line react/jsx-props-no-spreading
+					{...pageProps}
+				/>
+			</ApolloProvider>
+		);
+	}
+}
+
+// Wraps all components in the tree with the data provider
+export default withData(MyApp);
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
-import { Button } from '@blueprintjs/core';
-import Layout from '../components/Layout';
+import Head from 'next/head';
+import { useQuery } from '@apollo/react-hooks';
+import JOBS_QUERY from '../graphql/jobs.query';
 
-const Index = () => (
-	<Layout>
-		<p>Main</p>
-		<Button intent="success" text="button content" />
-	</Layout>
-);
+const Home = () => {
+	// Create a query hook
+	const { data, loading, error } = useQuery(JOBS_QUERY);
 
-export default Index;
+	if (loading) {
+		return <p>Loading...</p>;
+	}
+
+	if (error) {
+		return <p>Error: {JSON.stringify(error)}</p>;
+	}
+	return (
+		<div>
+			<Head>
+				<title>Home</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1>Simple Budget</h1>
+			<ul>
+				{data.jobs.map((job) => {
+					return <li key={`job__${job.id}`}>{job.title}</li>;
+				})}
+			</ul>
+		</div>
+	);
+};
+
+export default Home;
