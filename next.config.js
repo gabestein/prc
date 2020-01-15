aa341abd9dc6788349969a291f00d6b446d83007
Gabriel Stein <g@gabestein.com>
2020-01-14 19:58:51 -0500

    new: Add blueprint

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPlugins = require('next-compose-plugins');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withPlugins([
+	[
+		withSass,
+		{
+			cssLoaderOptions: {
+				url: false,
+			},
+		},
+	],
+]);
