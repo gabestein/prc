7c35b63d064bd0e0bfd4c1ef5abe461d34e6ff6c
Gabriel Stein <g@gabestein.com>
2020-02-09 10:56:15 -0500

    update: Update template

--- components/_template/Template.js
+++ components/_template/Template.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import './template.scss';
 
 const Template = (props) => {
-	return <div className="summary">Summary</div>;
+	return <div className="template">Template</div>;
 };
 
 Template.propTypes = {};
